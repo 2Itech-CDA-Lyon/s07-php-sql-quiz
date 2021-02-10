5c97dc3e4b1d2ef947190029bafd818b3bea25e1
Romain Tello <romain.tello@gmail.com>
2021-02-10 10:33:01 +0100

    Improve Answer model

--- models/answer.php
+++ models/answer.php
@@ -1,40 +1,85 @@
 <?php
 
 class Answer{
-    private string $text;
+    /**
+     * Database ID
+     * @var int
+     */
     private int $id;
+    /**
+     * Text to display
+     * @var string
+     */
+    private string $text;
+    /**
+     * Related question database ID
+     */
     private int $questionId;
 
-    function __construct(string $text, int $id, int $questionId){
-        $this->text   = $text; 
-        $this->id        = $id;
+    /**
+     * Create new anszwer
+     *
+     * @param integer $id Database ID
+     * @param string $text Text to display
+     * @param integer $questionId Related question database ID
+     */
+    function __construct(int $id, string $text, int $questionId){
+        $this->id = $id;
+        $this->text = $text; 
         $this->questionId= $questionId;
-    } 
-
-    function getText(){
-        return $this->text;
     }
 
-    function getid(){
+    /**
+     * Get database ID
+     *
+     * @return  int
+     */ 
+    public function getId()
+    {
         return $this->id;
     }
 
-    function getQuestionId(){
+    /**
+     * Get text to display
+     *
+     * @return  string
+     */ 
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set text to display
+     *
+     * @param  string  $text  Text to display
+     *
+     * @return  self
+     */ 
+    public function setText(string $text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get related question database ID
+     */ 
+    public function getQuestionId()
+    {
         return $this->questionId;
     }
 
-    function setText($text){
-        $this->text = $text;
-    }
-
-    function setId($id){
-        $this->id = $id;
-    }
-
-    function setQuestionId($questionId){
+    /**
+     * Set related question database ID
+     *
+     * @return  self
+     */ 
+    public function setQuestionId($questionId)
+    {
         $this->questionId = $questionId;
+
+        return $this;
     }
-
 }
-
-?>
